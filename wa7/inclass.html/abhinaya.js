bc495a0d102a7ee1f6f4df1bf2f5dcef91ff959e
ratnaunni <raun2297@colorado.edu>
2025-09-30 17:02:38 -0600

    commit

---
+++ wa7/inclass.html/abhinaya.js
@@ -0,0 +1,69 @@
+
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+const expandBtn = document.querySelector('.expand-btn');
+const details = document.querySelector('.card-content');
+let info = false;
+function showMenu() {
+    var shown = navMenu.classList.toggle("show");
+    navMenu.classList.toggle("hide");
+
+    if (shown) {
+        navToggle.setAttribute("aria-expanded", "true");
+        navToggle.classList.toggle("hide");
+    } else {
+        navToggle.setAttribute("aria-expanded", "false");
+        navToggle.style.transform = "rotate(0deg)";
+    }
+}
+function checkKey(key_code) {
+    if (key_code === 32) {
+        showMenu();
+        console.log("worked");
+    }
+}
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('.carousel-container')
+    const grid = document.querySelector('.carousel-grid');
+    const firstSlide = grid.querySelector('.carousel-item');
+    const nextbtn = document.querySelector('.car-btn.next');
+    const prevBtn = document.querySelector('.car-btn.prev');
+
+    if (!container || !grid || !firstSlide || !nextbtn || !prevBtn) {
+        console.error('Carousel elements missing.');
+        return;
+    }
+    function getScrollAmount() {
+        const slideWidth = firstSlide.getBoundingClientRect().width;
+        const styles = getComputedStyle(grid);
+        const gap = parseFloat(styles.gap) || 0;
+        return slideWidth + gap;
+    }
+    function scrollByAmount(dir = 1) {
+        container.scrollBy({ left: dir * getScrollAmount(), behavior: 'smooth' });
+    }
+
+    nextbtn.addEventListener('click', () => scrollByAmount(1));
+    prevBtn.addEventListener('click', () => scrollByAmount(-1));
+})
+let themebtn = document.querySelector('#theme');
+themebtn.addEventListener('click', changeTheme);
+// Save user's theme choice
+function changeTheme() {
+    let ogTheme = localStorage.getItem('userTheme') || 'light';
+
+    let newTheme = ogTheme === 'light' ? 'dark' : 'light';
+
+    localStorage.setItem('userTheme', newTheme);
+    document.body.className = newTheme;
+
+    themebtn.textContent = newTheme == 'light' ? "Switch to Dark Mode" : 'Switch to Light Mode';
+}
+// Load saved theme on page load
+window.addEventListener('load', function () {
+    const savedTheme = localStorage.getItem('userTheme') || 'light';
+    document.body.className = savedTheme;
+
+    themebtn.textContent = savedTheme === 'light' ? "Switch to Dark Mode" : 'Switch to Light Mode';
+
+});
